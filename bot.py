af690a62ae2225217169c917bc25d5cd730b58a3
sintuvez31-web <sintuvez31@gmail.com>
2026-07-24 17:15:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import requests
-import os
 
-TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+TOKEN = "MTUzMDIxMjM4OTEwNTE3NjYzOA.GJN5XJ.wEGEMLz_vGeImmJDQtJJeiKRvG8idp5mtbizN8"
+CHANNEL_ID = "KANAL_ID"
 
 url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
 headers = {"Authorization": f"Bot {TOKEN}", "Content-Type": "application/json"}
-data = {"content": "rob" + "lox" + ".com" + ".ug"}  # Link parçalı, uyarıyı aşar
+data = {"content": "Test mesajı"}
 
 response = requests.post(url, headers=headers, json=data)
+print(response.status_code)
 print(response.json())
